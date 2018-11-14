0e686a4dac64580da320d18141618c228dbdcf79
pdn <pdn@qq.com>
2018-11-14 19:33:29 +0800

    第一次搭建工程

---
+++ src/main/java/test.java
@@ -0,0 +1,15 @@
+/**
+ * @Author pdn
+ * @Time 2018/11/14 19:29
+ * @Version 1.0
+ */
+public class test {
+
+    /**
+     * hahahhahahaha
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("这个一个Git远程操作实例！！");
+    }
+}
